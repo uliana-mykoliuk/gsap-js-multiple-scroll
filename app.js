92fb6882c4c2c276ab115d8fbe9f589bee61fa68
Uliana Mykoliuk <uliana.mykoliuk@gmail.com>
2024-05-29 21:54:56 +0300

    add animation

---
+++ app.js
@@ -0,0 +1,18 @@
+const mountainRange = document.querySelector(".home-mountains img");
+const title = document.querySelector("h1");
+
+gsap.to(mountainRange, {
+  y: 100,
+  scrollTrigger: {
+    trigger: "home-mountains",
+    scrub: true,
+  },
+});
+
+gsap.to(title, {
+  y: 500,
+  scrollTrigger: {
+    trigger: "home-mountains",
+    scrub: true,
+  },
+});
